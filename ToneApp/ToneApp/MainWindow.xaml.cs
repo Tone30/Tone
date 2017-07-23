e0117042fe4471acbdbcae669f3364ba2da2b457
Su <su-zz@qq.com>
2017-07-23 21:19:21 +0800

    新建项目

---
+++ ToneApp/ToneApp/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace ToneApp
+{
+    /// <summary>
+    ///     MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("OK");
+        }
+    }
+}
